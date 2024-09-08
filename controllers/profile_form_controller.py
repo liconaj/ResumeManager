fb11e99550614c713298718439db0308cf2c690a
Josué Licona <liconaj215@gmail.com>
2024-09-08 00:40:17 -0500

    Add helper to create line edit controllers

--- controllers/profile_form_controller.py
+++ controllers/profile_form_controller.py
@@ -1,6 +1,6 @@
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile, QIODevice, Qt, Slot
-from PySide6.QtWidgets import QWidget, QDialog, QComboBox
+from PySide6.QtCore import QFile, QIODevice, Slot
+from PySide6.QtWidgets import QDialog, QComboBox, QLineEdit
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -66,50 +66,54 @@ class ProfileFormController(QDialog):
         self.form.nextPagePushButton.setEnabled(self.current_page_index < self.form.stackedWidget.count() - 1)
         self.form.navigationLabel.setText(f"Página {self.current_page_index + 1} de {self.form.stackedWidget.count()}")
     
+    def _line_edit(self, line_edit: QLineEdit, column: str) -> LineEditController:
+        controller = LineEditController(line_edit, self.data, column)
+        return controller
+
     def setup_line_edits(self) -> None:
-        self.full_name = LineEditController(self.form.fullNameLineEdit, self.data, "full_name")
-        self.email = LineEditController(self.form.emailLineEdit, self.data, "email")
-        self.phone = LineEditController(self.form.phoneLineEdit, self.data, "phone")
-        self.other_phone = LineEditController(self.form.otherPhoneLineEdit, self.data, "other_phone")
-        self.id_num = LineEditController(self.form.idNumLineEdit, self.data, "id_document_number")
-        self.id_num_confirm = LineEditController(self.form.idNumConfirmLineEdit, self.data, "id_document_number_confirmation")
-        self.under_degree = LineEditController(self.form.underDegreeLineEdit, self.data, "undergraduate_degree")
-        self.under_institution = LineEditController(self.form.underInstitutionLineEdit, self.data, "undergraduate_institution")
-        self.degree_1_name = LineEditController(self.form.deg1NameLineEdit, self.data, "degree_1_name")
-        self.degree_2_name = LineEditController(self.form.deg2NameLineEdit, self.data, "degree_2_name")
-        self.degree_3_name = LineEditController(self.form.deg3NameLineEdit, self.data, "degree_3_name")
-        self.linkedin = LineEditController(self.form.linkedInLineEdit, self.data, "linkedin")
-        self.company = LineEditController(self.form.companyLineEdit, self.data, "company")
+        self.full_name = self._line_edit(self.form.fullNameLineEdit, "full_name")
+        self.email = self._line_edit(self.form.emailLineEdit, "email")
+        self.phone = self._line_edit(self.form.phoneLineEdit, "phone")
+        self.other_phone = self._line_edit(self.form.otherPhoneLineEdit, "other_phone")
+        self.id_num = self._line_edit(self.form.idNumLineEdit, "id_document_number")
+        self.id_num_confirm = self._line_edit(self.form.idNumConfirmLineEdit, "id_document_number_confirmation")
+        self.under_degree = self._line_edit(self.form.underDegreeLineEdit, "undergraduate_degree")
+        self.under_institution = self._line_edit(self.form.underInstitutionLineEdit, "undergraduate_institution")
+        self.degree_1_name = self._line_edit(self.form.deg1NameLineEdit, "degree_1_name")
+        self.degree_2_name = self._line_edit(self.form.deg2NameLineEdit, "degree_2_name")
+        self.degree_3_name = self._line_edit(self.form.deg3NameLineEdit, "degree_3_name")
+        self.linkedin = self._line_edit(self.form.linkedInLineEdit, "linkedin")
+        self.company = self._line_edit(self.form.companyLineEdit, "company")
     
     def setup_plain_texts(self) -> None:
         self.professional_profile = PlainTextEditController(self.form.professionalProfilePlainTextEdit, self.data, "professional_profile")
         self.role_description = PlainTextEditController(self.form.roleDescriptionPlainTextEdit, self.data, "role_description")
     
-    def _setup_combobox(self, combobox: QComboBox, column: str, option: str = None) -> ComboBoxController:
+    def _combobox(self, combobox: QComboBox, column: str, option: str = None) -> ComboBoxController:
         option = column if option is None else option
         controller = ComboBoxController(combobox, get_option(option), self.data, column)
         return controller
 
     def setup_comboboxes(self) -> None:
-        self.id_type = self._setup_combobox(self.form.idTypeComboBox, "id_document_type")
-        self.disability = self._setup_combobox(self.form.disabilityComboBox, "disability_condition")
-        self.english = self._setup_combobox(self.form.englishComboBox, "english_level", "language_level")
-        self.french = self._setup_combobox(self.form.frenchComboBox, "french_level", "language_level")
-        self.portuguese = self._setup_combobox(self.form.portugueseComboBox, "portuguese_level", "language_level")
-        self.other_languages = self._setup_combobox(self.form.otherLanguagesComboBox, "other_languages_level", "language_level")
-        self.degree_1 = self._setup_combobox(self.form.deg1LevelComboBox, "degree_1", "degree")
-        self.degree_1_status = self._setup_combobox(self.form.deg1StatusComboBox, "degree_1_status", "degree_status")
-        self.degree_2 = self._setup_combobox(self.form.deg2LevelComboBox, "degree_2", "degree")
-        self.degree_2_status = self._setup_combobox(self.form.deg2StatusComboBox, "degree_2_status", "degree_status")
-        self.degree_3 = self._setup_combobox(self.form.deg3LevelComboBox, "degree_3", "degree")
-        self.degree_3_status = self._setup_combobox(self.form.deg3StatusComboBox, "degree_3_status", "degree_status")
-        self.mv_program_1 = self._setup_combobox(self.form.mv1NameComboBox, "mv_program_1", "mv_program")
-        self.mv_program_2 = self._setup_combobox(self.form.mv2NameComboBox, "mv_program_2", "mv_program")
-        self.mv_program_3 = self._setup_combobox(self.form.mv3NameComboBox, "mv_program_3", "mv_program")
-        self.sector = self._setup_combobox(self.form.sectorComboBox, "sector")
-        self.role = self._setup_combobox(self.form.roleComboBox, "role")
-        self.experience_sector = self._setup_combobox(self.form.expSectorComboBox, "experience_sector", "sector")
-        self.experience_duration = self._setup_combobox(self.form.expYearsComboBox, "experience_duration")
+        self.id_type = self._combobox(self.form.idTypeComboBox, "id_document_type")
+        self.disability = self._combobox(self.form.disabilityComboBox, "disability_condition")
+        self.english = self._combobox(self.form.englishComboBox, "english_level", "language_level")
+        self.french = self._combobox(self.form.frenchComboBox, "french_level", "language_level")
+        self.portuguese = self._combobox(self.form.portugueseComboBox, "portuguese_level", "language_level")
+        self.other_languages = self._combobox(self.form.otherLanguagesComboBox, "other_languages_level", "language_level")
+        self.degree_1 = self._combobox(self.form.deg1LevelComboBox, "degree_1", "degree")
+        self.degree_1_status = self._combobox(self.form.deg1StatusComboBox, "degree_1_status", "degree_status")
+        self.degree_2 = self._combobox(self.form.deg2LevelComboBox, "degree_2", "degree")
+        self.degree_2_status = self._combobox(self.form.deg2StatusComboBox, "degree_2_status", "degree_status")
+        self.degree_3 = self._combobox(self.form.deg3LevelComboBox, "degree_3", "degree")
+        self.degree_3_status = self._combobox(self.form.deg3StatusComboBox, "degree_3_status", "degree_status")
+        self.mv_program_1 = self._combobox(self.form.mv1NameComboBox, "mv_program_1", "mv_program")
+        self.mv_program_2 = self._combobox(self.form.mv2NameComboBox, "mv_program_2", "mv_program")
+        self.mv_program_3 = self._combobox(self.form.mv3NameComboBox, "mv_program_3", "mv_program")
+        self.sector = self._combobox(self.form.sectorComboBox, "sector")
+        self.role = self._combobox(self.form.roleComboBox, "role")
+        self.experience_sector = self._combobox(self.form.expSectorComboBox, "experience_sector", "sector")
+        self.experience_duration = self._combobox(self.form.expYearsComboBox, "experience_duration")
 
     @Slot()
     def next_page(self) -> None:
